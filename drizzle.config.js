5f22af5968958d89906f8c1c7ee9d72c2fde9d34
MEET1733 <meetharsoda5@gmail.com>
2024-07-22 03:44:14 +0530

    Added-react-web-cam

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-        url: 'postgresql://InterviewAI_owner:bHIA6DsYo8jQ@ep-nameless-cake-a1sjedah.ap-southeast-1.aws.neon.tech/InterviewAI?sslmode=require',
+        url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
     }
 };
