1b15d9f122334d277935530dd31442b24c8d37d2
MEET1733 <meetharsoda5@gmail.com>
2024-07-23 15:35:26 +0530

    Generated-Feedback-of-user-answer-from-Gemini

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnsSection.jsx
@@ -3,35 +3,78 @@ import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import Webcam from 'react-webcam'
-import useSpeechToText from 'react-hook-speech-to-text';
 import { Mic } from 'lucide-react'
+import { toast } from 'sonner'
+import { chatSession } from '@/utils/GeminiAIModel'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { db } from '@/utils/db'
+import { UserAnswer } from '@/utils/schema'
+import { useUser } from '@clerk/nextjs'
+import moment from 'moment'
 
-function RecordAnsSection() {
+function RecordAnsSection({ mockInterviewQuestion, activeQuestionIndex, interviewData }) {
     const [userAnswer, setuserAnswer] = useState('');
+    const { user } = useUser();
+    const [loading, setLoading] = useState(false);
+
     const {
-        error,
-        interimResult,
-        isRecording,
-        results,
-        startSpeechToText,
-        stopSpeechToText,
-    } = useSpeechToText({
-        continuous: true,
-        useLegacyResults: false
-    });
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition();
 
     useEffect(() => {
-        results.map((result) => (
-            setuserAnswer(prevAns => prevAns + result?.transcript)
-        ))
-    }, [results])
+        setuserAnswer(transcript)
+    }, [transcript])
 
-    const SaveUserAnswer = () => {
-        if (isRecording) {
-            stopSpeechToText();
+    const SaveUserAnswer = async () => {
+        if (listening) {
+            SpeechRecognition.stopListening();
+            setLoading(true);
+
+            if (userAnswer.length < 10) {
+                toast('Error while saving your answer, Please record again');
+                setLoading(false);
+                setuserAnswer('');
+                return;
+            }
+
+            const feedbackPrompt = "Question:" + mockInterviewQuestion[activeQuestionIndex]?.question +
+                ", User Answer:" + userAnswer + ", Depends on question and user answer for given interview question" +
+                " please give us rating for answer and feedback as area of improvement if any " +
+                "in just 3 to 5 lines to improve it in JSON format with rating field and feedback field ";
+
+            const result = await chatSession.sendMessage(feedbackPrompt);
+            const mockJsonResp = (result.response.text()).replace('```json', '').replace('```', '');
+            console.log(mockJsonResp);
+            const JsonFeedbackResp = JSON.parse(mockJsonResp);
+
+            const resp = await db.insert(UserAnswer).values({
+                mockIdRef: interviewData?.mockId,
+                question: mockInterviewQuestion[activeQuestionIndex]?.question,
+                correctAnswer: mockInterviewQuestion[activeQuestionIndex]?.answer,
+                userAnswer: userAnswer,
+                feedback: JsonFeedbackResp?.feedback,
+                rating: JsonFeedbackResp?.rating,
+                userEmail: user?.primaryEmailAddress.emailAddress,
+                createdAt: moment().format('DD-MM-yyyy')
+            })
+
+            if (resp) {
+                toast('User Answer recorded sussceefully')
+            }
+            setLoading(false);
+            setuserAnswer('');
         } else {
-            startSpeechToText();
+            SpeechRecognition.startListening();
         }
+    }
+
+
+    const RestartRecording = () => {
+        resetTranscript();
+        setuserAnswer('');
     }
 
     return (
@@ -48,16 +91,28 @@ function RecordAnsSection() {
                 />
             </div>
 
-            <Button
-                variant="outline"
-                className="my-10"
-                onClick={SaveUserAnswer}
-            >
-                {isRecording ? <h2 className='text-red-600 flex gap-2 items-center'><Mic />Stop Recording</h2>
-                    : <h2 className='text-primary flex gap-2 items-center'><Mic />Record Answer</h2>}
-            </Button>
+            <div className='flex items-center gap-5'>
+                <Button
+                    variant="outline"
+                    className="my-10"
+                    onClick={SaveUserAnswer}
+                    disabled={loading}
+                >
+                    {loading && <h2>Submitting Answer...</h2>}
+                    {!loading && listening ? <h2 className='text-red-600 flex gap-2 items-center'><Mic />Stop Recording</h2>
+                        : !loading && <h2 className='text-primary flex gap-2 items-center'><Mic />Record Answer</h2>}
+                </Button>
 
-            <Button onClick={() => console.log(userAnswer)}>Show User Answer</Button>
+                {listening &&
+                    <Button
+                        variant="outline"
+                        className="my-10"
+                        onClick={RestartRecording}
+                    >
+                        Restart
+                    </Button>}
+            </div>
+
         </div>
 
     )
--- app/dashboard/interview/[interviewId]/start/page.jsx
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -5,6 +5,8 @@ import { eq } from 'drizzle-orm';
 import React, { useEffect, useState } from 'react'
 import QuestionsSection from './_components/QuestionsSection';
 import RecordAnsSection from './_components/RecordAnsSection';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 function StartInterview({ params }) {
 
@@ -26,16 +28,34 @@ function StartInterview({ params }) {
     }
 
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
-            {/* Questions */}
-            <QuestionsSection
-                mockInterviewQuestion={mockInterviewQuestion}
-                activeQuestionIndex={activeQuestionIndex}
-            />
+        <div>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+                {/* Questions */}
+                <QuestionsSection
+                    mockInterviewQuestion={mockInterviewQuestion}
+                    activeQuestionIndex={activeQuestionIndex}
+                />
 
-            {/* Video/Audio Recording */}
-            <RecordAnsSection />
+                {/* Video/Audio Recording */}
+                <RecordAnsSection
+                    mockInterviewQuestion={mockInterviewQuestion}
+                    activeQuestionIndex={activeQuestionIndex}
+                    interviewData={interviewData}
+                />
+            </div>
+
+            <div className='flex justify-end gap-6'>
+                {activeQuestionIndex > 0 &&
+                    <Button onClick={() => setActiveQuestionIndex(activeQuestionIndex - 1)}>Previous Question</Button>}
+                {activeQuestionIndex != mockInterviewQuestion?.length - 1 &&
+                    <Button onClick={() => setActiveQuestionIndex(activeQuestionIndex + 1)}>Next Question</Button>}
+                {activeQuestionIndex == mockInterviewQuestion?.length - 1 &&
+                    <Link href={'/dashboard/interview/' + interviewData?.mockId + "/feedback"}>
+                        <Button>End Interview</Button>
+                    </Link>}
+            </div>
         </div>
+
     )
 }
 
